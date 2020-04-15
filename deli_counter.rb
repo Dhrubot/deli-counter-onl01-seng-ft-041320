ba1b48e19d18b1349c36245d5a72472c429618e1
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-15 20:43:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,8 @@ def line(array)
   puts "The line is currently empty." unless !array.empty?
   str = "The line is currently:"
   array.each_with_index do |name, i|
-    "The line is currently: #{i +1}. #{ name }Ada 2. Grace 3. Kent"
+    str << "#{i +1}. #{ name }"
   end
+  str
 end
   
