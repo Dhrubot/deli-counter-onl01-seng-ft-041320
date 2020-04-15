1766fec21d26689efd1d3bb1ae51d9d8551543e3
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-15 20:38:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,7 @@
 def line(array) 
   
   puts "The line is currently empty." unless !array.empty?
+  
   array.each_with_index do |name, i|
     "The line is currently: #{i +1}. #{ name }Ada 2. Grace 3. Kent"
   end
